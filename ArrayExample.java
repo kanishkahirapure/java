a4a99171c67476c96db4e0207941dbbd6b8982b4
Kanishka Hirapure <kannuhirapure72@gmail.com>
2025-05-19 20:50:02 +0530

    write array examples

--- ArrayExample.java
+++ ArrayExample.java
@@ -182,21 +182,57 @@ public class ArrayExample{
 
 
 
-    public static void maxSubarraySum(int numbers[]){
+    // public static void maxSubarraySum(int numbers[]){
+    //     int currSum = 0;
+    //     int maxSum = Integer.MIN_VALUE;
+    //     int prefix[] = new int [numbers.length];
+
+    //     prefix[0] = numbers[0];
+    //     //calculare prefix array
+    //     for(int i=1; i<prefix.length; i++){
+    //         prefix[i] = prefix[i-1] + numbers[i];
+    //     }
+
+    //     for(int i=0; i<numbers.length; i++){
+    //         int start = i;
+    //         for(int j=i; j<numbers.length; j++){
+    //             int end = j;
+
+    //             currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+
+    //             if(maxSum < currSum){
+    //                 maxSum = currSum;
+    //             }
+    //         }
+    //     }
+
+    //     System.out.println("Max sum ="+ maxSum);
+    // }
+
+//     public static void main(String args[]){
+//         int numbers[] = {1, -2, 6, -1, 3};
+//         maxSubarraySum(numbers);
+//     }
+
+
+public static void maxSubarraySum(int numbers[]){
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int [numbers.length];
 
+        prefix[0] = numbers[0];
+        //calculare prefix array
+        for(int i=1; i<prefix.length; i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+        }
 
         for(int i=0; i<numbers.length; i++){
             int start = i;
             for(int j=i; j<numbers.length; j++){
                 int end = j;
-                currSum = 0;
-                for(int k=start; k<=end; k++){//print
-                    //subarrays sum
-                    currSum += numbers[k];
-                }
-                System.out.println(currSum );//subarrays
+
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+
                 if(maxSum < currSum){
                     maxSum = currSum;
                 }
@@ -206,9 +242,25 @@ public class ArrayExample{
         System.out.println("Max sum ="+ maxSum);
     }
 
-    public static void main(String args[]){
-        int numbers[] = {2, 4, 6, 8, 10};
-        maxSubarraySum(numbers);
+    public static void kadanes(int numbers[]){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0; i<numbers.length; i++){
+            cs = cs + numbers[i];
+            if(cs < 0){
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+
+        System.out.println("our max subarray sum is : "+ ms);
     }
 
- }
+    public static void main(String args[]){
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        kadanes(numbers);
+    }
+    
+
+  }
